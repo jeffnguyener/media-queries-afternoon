adeb1c87503713c8e685bdbbf1855480fbf93cf8
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-07-07 13:16:34 -0600

    update app & added bg

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Portfolio from './Components/Portfolio';
-import Services from './Components/Services';
-import About from './Components/About';
-import Team from './Components/Team';
-import Contact from './Components/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <Services />
-      <Portfolio />
-      <About />
-      <Team />
-      <Contact />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false
+    };
+
+  }
+
+  showMenu = () => {
+    this.setState({showMenu: !this.state.showMenu});
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <div className="Background">
+        <nav>
+
+          <div className='container'>
+            <div className='title'>Start Bootstrap</div>
+            <ul className='nav-links'>
+              <li>Services</li>
+              <li>Portfolio</li>
+              <li>About</li>
+              <li>Team</li>
+              <li>Contact</li>
+            </ul>
+            <button className='nav-button' onClick={this.showMenu}>Menu</button>
+          </div>
+            <div className={this.state.showMenu ? 'nav-menu nav-menu-show' : 'nav-menu'}>
+              <ul className='nav-list'>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>About</li>
+                <li>Team</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+        </nav>
+      <header>
+        <div className='header-container'>
+        <div className='welcome'>Welcome to Our Studio!</div>
+        <div className='meet'>It's nice to meet you</div>
+        <button className='contact'>Tell Me More</button>
+          </div>
+      </header>
+        </div>
+      <section>
+      </section>
+      </div>
+    );
+  }
 }
-
 export default App;
